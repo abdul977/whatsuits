a7f48871c1346a197a44483391239f6a2b640625
abdul977 <abdulmuminibrahim74@gmail.com>
2025-02-22 19:44:03 +0100

    starting

--- Unexpected-Keyboard-master/srcs/juloo.keyboard2/Config.java
+++ Unexpected-Keyboard-master/srcs/juloo.keyboard2/Config.java
@@ -235,6 +235,7 @@ public final class Config
           return R.style.MonetLight;
         return R.style.MonetDark;
       case "rosepine": return R.style.RosePine;
+      case "moderndark": return R.style.ModernDark;
       default:
       case "system":
         if ((night_mode & Configuration.UI_MODE_NIGHT_NO) != 0)
